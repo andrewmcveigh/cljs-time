2f0315d6c4367327027b9b12ab2d483c58b11c9a
Andrew Mcveigh <me@andrewmcveigh.com>
2013-09-02 13:16:42 +0200

    Add coerce_test.cljs

---
+++ test/cljs_time/coerce_test.cljs
@@ -0,0 +1,88 @@
+(ns clj-time.coerce-test
+  (:refer-clojure :exclude [= extend second])
+  (:require-macros
+    [cljs-test.macros :refer [is deftest]])
+  (:require
+    [cljs-time.core :refer [= date-time date-midnight plus hours]]
+    [cljs-time.coerce :refer
+     [from-date from-long from-string to-date to-date-time to-epoch to-long
+      to-string]]))
+
+(deftest test-from-date
+  (let [dt (from-long 893462400000)
+        d  (to-date dt)]
+    (is (instance? js/Date d))
+    (is (= dt (from-date d)))))
+
+(deftest test-from-long
+  (is (= (date-time 1998 4 25) (from-long 893462400000))))
+
+(deftest test-from-string
+  (is (= (from-string "1998-04-25T00:00:00.000Z")
+         (date-time 1998 4 25))))
+
+;(deftest test-from-year-month
+  ;(is (= (to-date-time (year-month 1998 4))
+         ;(date-time 1998 4))))
+
+;(deftest test-from-local-date
+  ;(is (= (to-date-time (local-date 2013 03 20))
+         ;(date-time 2013 03 20))))
+
+(deftest test-to-date
+  (is (nil? (to-date nil)))
+  (is (nil? (to-date "")))
+  (is (nil? (to-date "x")))
+  (is (= (js/Date. 893462400000) (to-date (date-time 1998 4 25))))
+  (is (= (js/Date. 893462400000) (to-date (date-midnight 1998 4 25))))
+  (is (= (js/Date. 893462400000) (to-date (js/Date. 893462400000))))
+  (is (= (js/Date. (long 0)) (to-date 0)))
+  (is (= (js/Date. 893462400000) (to-date 893462400000)))
+  (is (= (js/Date. 893462400000) (to-date "1998-04-25T00:00:00.000Z"))))
+
+(deftest test-to-date-time
+  (is (nil? (to-date-time nil)))
+  (is (nil? (to-date-time "")))
+  (is (nil? (to-date-time "x")))
+  (is (= (date-time 1998 4 25) (to-date-time (date-time 1998 4 25))))
+  (is (= (date-midnight 1998 4 25) (to-date-time (date-time 1998 4 25))))
+  (is (= (date-time 2000 1 1 1) (plus (to-date-time (date-midnight 2000 1 1)) (hours 1))))
+  (is (= (date-time 1998 4 25) (to-date-time (js/Date. 893462400000))))
+  (is (= (date-time 1970 1 1) (to-date-time 0)))
+  (is (= (date-time 1998 4 25) (to-date-time 893462400000)))
+  (is (= (date-time 1998 4 25) (to-date-time "1998-04-25T00:00:00.000Z"))))
+
+(deftest test-to-long
+  (is (nil? (to-long nil)))
+  (is (nil? (to-long "")))
+  (is (nil? (to-long "x")))
+  (is (= 893462400000 (to-long (date-time 1998 4 25))))
+  (is (= 0 (to-long (date-midnight 1970))))
+  (is (= (to-long (date-time 1993 3 15)) (to-long (date-midnight 1993 3 15))))
+  (is (= 893462400000 (to-long (js/Date. 893462400000))))
+  (is (= (long 0) (to-long 0)))
+  (is (= 893462400000 (to-long 893462400000)))
+  (is (= 893462400000 (to-long "1998-04-25T00:00:00.000Z"))))
+
+(deftest test-to-epoch
+  (is (nil? (to-epoch nil)))
+  (is (nil? (to-epoch "")))
+  (is (nil? (to-epoch "x")))
+  (is (= 893462400 (to-epoch (date-time 1998 4 25))))
+  (is (= 0 (to-epoch (date-midnight 1970))))
+  (is (= (to-epoch (date-time 1993 3 15)) (to-epoch (date-midnight 1993 3 15))))
+  (is (= 893462400 (to-epoch (js/Date. 893462400000))))
+  (is (= (long 0) (to-epoch 0)))
+  (is (= 893462400 (to-epoch 893462400000)))
+  (is (= 893462400 (to-epoch "1998-04-25T00:00:00.000Z"))))
+
+(deftest test-to-string
+  (is (nil? (to-string nil)))
+  (is (nil? (to-string "")))
+  (is (nil? (to-string "x")))
+  (is (= "1998-04-25T00:00:00.000Z" (to-string (date-time 1998 4 25))))
+  (is (= "1998-04-25T00:00:00.000Z" (to-string (date-midnight 1998 4 25))))
+  (is (= "1998-04-25T00:00:00.000Z" (to-string (js/Date. 893462400000))))
+  (is (= "1970-01-01T00:00:00.000Z" (to-string 0)))
+  (is (= "1998-04-25T00:00:00.000Z" (to-string 893462400000)))
+  (is (= "1998-04-25T00:00:00.000Z" (to-string "1998-04-25T00:00:00.000Z"))))
