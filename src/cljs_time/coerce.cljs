0915bed5c3ffd4975e626f624b622e79111041e8
Andrew Mcveigh <me@andrewmcveigh.com>
2013-09-02 12:36:36 +0200

    Add in clj-time.coerce

--- src/cljs_time/coerce.cljs
+++ src/cljs_time/coerce.cljs
@@ -1,9 +1,89 @@
 (ns cljs-time.coerce
+  "Utilites to coerce Joda DateTime instances to and from various other types.
+  For example, to convert a Joda DateTime to and from a Java long:
+
+  => (to-long (date-time 1998 4 25))
+  893462400000
+
+  => (from-long 893462400000)
+  #<DateTime 1998-04-25T00:00:00.000Z>"
+  (:refer-clojure :exclude [extend second])
   (:require
-    [goog.date :as date]))
+    [cljs-time.core :refer []]
+    [cljs-time.format :as time-fmt]
+    [goog.date :as date]
+    ))
 
-(defn to-date [d]
-  (js/Date. (.getTime d)))
+(defprotocol ICoerce
+  (to-date-time [obj] "Convert `obj` to a goog.date.DateTime instance."))
 
-(defn from-date [date]
-  (doto (date/UtcDateTime.) (.setTime (.getTime date))))
+(defn from-long
+  "Returns a DateTime instance in the UTC time zone corresponding to the given
+  number of milliseconds after the Unix epoch."
+  [millis]
+  (doto (date/UtcDateTime.) (.setTime millis)))
+
+(defn from-string
+  "Returns DateTime instance from string using formatters in clj-time.format,
+  returning first which parses"
+  [s]
+  (first
+   (for [f (vals time-fmt/formatters)
+         :let [d (try (time-fmt/parse f s) (catch Exception _ nil))]
+         :when d] d)))
+
+(defn from-date
+  "Returns a DateTime instance in the UTC time zone corresponding to the given
+Java Date object."
+  [date]
+  (from-long (.getTime date)))
+
+(defn to-long
+  "Convert `obj` to the number of milliseconds after the Unix epoch."
+  [obj]
+  (if-let [dt (to-date-time obj)]
+    (.getTime dt)))
+
+(defn to-epoch
+  "Convert `obj` to Unix epoch."
+  [obj]
+  (let [millis (to-long obj)]
+    (and millis (/ millis 1000))))
+
+(defn to-date
+  "Convert `obj` to a JavaScript Date instance."
+  [obj]
+  (if-let [dt (to-date-time obj)]
+    (js/Date. (.getTime dt))))
+
+(defn to-string
+  "Returns a string representation of obj in UTC time-zone
+using (ISODateTimeFormat/dateTime) date-time representation."
+  [obj]
+  (if-let [dt (to-date-time obj)]
+    (time-fmt/unparse (:date-time time-fmt/formatters) dt)))
+
+(extend-protocol ICoerce
+  nil
+  (to-date-time [_]
+    nil)
+
+  js/Date
+  (to-date-time [date]
+    (from-date date))
+
+  goog.date.DateTime
+  (to-date-time [date-time]
+    date-time)
+
+  goog.date.Date
+  (to-date-time [date-midnight]
+    (doto date-midnight (.set date-midnight)))
+
+  js/Number
+  (to-date-time [long]
+    (from-long long))
+
+  js/String
+  (to-date-time [string]
+    (from-string string)))
