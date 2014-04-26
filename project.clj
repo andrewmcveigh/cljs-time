ffa2dc2c39f4ffc788e4c8065b340c56cd6e4664
Andrew Mcveigh <me@andrewmcveigh.com>
2014-04-26 13:46:10 +0200

    Bump to 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.andrewmcveigh/cljs-time "0.1.4"
+(defproject com.andrewmcveigh/cljs-time "0.1.5-SNAPSHOT"
   :description "A clj-time inspired date library for clojurescript."
   :url "https://github.com/andrewmcveigh/cljs-time"
   :license {:name "Eclipse Public License"
