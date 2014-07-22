055103f8ac1bfb3197d0d86d66144d77103dc836
Andrew Mcveigh <me@andrewmcveigh.com>
2014-07-22 15:59:17 +0200

    Release 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.andrewmcveigh/cljs-time "0.1.6-SNAPSHOT"
+(defproject com.andrewmcveigh/cljs-time "0.1.6"
   :description "A clj-time inspired date library for clojurescript."
   :url "https://github.com/andrewmcveigh/cljs-time"
   :license {:name "Eclipse Public License"
