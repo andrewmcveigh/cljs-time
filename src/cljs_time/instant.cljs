836408d318ef1cc44ee56cf6f273117d306affbf
Andrew Mcveigh <me@andrewmcveigh.com>
2016-01-07 19:13:15 +0000

    Fix bad format alias in instant.cljs

--- src/cljs_time/instant.cljs
+++ src/cljs_time/instant.cljs
@@ -1,20 +1,20 @@
 (ns cljs-time.instant
   (:require
    [goog.date.DateTime]
-   [cljs-time.format :as fmt]))
+   [cljs-time.format :refer [unparse formatters]]))
 
 (extend-protocol IPrintWithWriter
   goog.date.UtcDateTime
   (-pr-writer [obj writer opts]
     (-write writer "#inst ")
-    (pr-writer (tf/unparse (:date-time tf/formatters) obj) writer opts))
+    (pr-writer (unparse (:date-time formatters) obj) writer opts))
 
   goog.date.DateTime
   (-pr-writer [obj writer opts]
     (-write writer "#inst ")
-    (pr-writer (tf/unparse (:date-time tf/formatters) obj) writer opts))
+    (pr-writer (unparse (:date-time formatters) obj) writer opts))
 
   goog.date.Date
   (-pr-writer [obj writer opts]
     (-write writer "#inst ")
-    (pr-writer (tf/unparse (:date tf/formatters) obj) writer opts)))
+    (pr-writer (unparse (:date formatters) obj) writer opts)))
